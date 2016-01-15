bcb4d24a5884e9055434b84a6f314445f09c624f
Jan Wielemaker <J.Wielemaker@cs.vu.nl>
2016-01-15 21:17:28 +0100

    Prepare for installation

--- clingo.pl
+++ clingo.pl
@@ -1,3 +1,37 @@
+/*  Part of SWI-Prolog
+
+    Author:        Jan Wielemaker and Roland Kaminski
+    E-mail:        J.Wielemaker@vu.nl
+    WWW:           http://www.swi-prolog.org
+    Copyright (c)  2015, VU University Amsterdam
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions
+    are met:
+
+    1. Redistributions of source code must retain the above copyright
+       notice, this list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in
+       the documentation and/or other materials provided with the
+       distribution.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+    FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+    COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+    INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+    BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+    LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+    ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+    POSSIBILITY OF SUCH DAMAGE.
+*/
+
 :- module(clingo,
 	  [ clingo_new/2,		% -Clingo, +Options
 	    clingo_add/3,		% +Clingo, +ProgAndParams, +String
@@ -18,17 +52,18 @@
 	    op(100, xfx, {}),
 	    op(100, fx, #),
 	    op(100, fx, ~),
+	    op(200, fy, @),
 	    op(1100, xfx, const),
 	    op(1100, xfx, show),
 	    op(1100, xfx, external)
 	  ]).
-:- use_foreign_library(clingo).
+:- use_foreign_library(foreign(clingo)).
 :- use_module(library(dcg/basics)).
 :- use_module(library(apply)).
 :- use_module(library(yall)).
 :- use_module(library(error)).
 :- use_module(library(debug)).
-:- use_module(library(apply_macros)).
+:- use_module(library(apply_macros), []).
 
 :- meta_predicate
 	clingo_model(:,-).
@@ -37,6 +72,10 @@
 
 @author Roland Kaminski and Jan Wielemaker
 */
+
+% Although the interface is licensed under the BSD license, the
+% loaded Clingo library is covered by the GPL
+:- license(gpl).
 
 :- public inject_values/3.
 
